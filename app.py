b5bbc2eeb10bb2d0de4680beb8f962871c4cffd8
sinanemre-dev <sinanemregulsen4048@gmail.com>
2025-11-22 15:45:04 +0300

    Mesaj guncellendi - Otomasyon testi

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Bu guvenli bir PYTHON konteyneridir."
+    return "Bu guvenli bir PYTHON konteyneridir.şimdi imzalama olduğu anlaşılsın diye yeni bir şey yapıyorum"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
